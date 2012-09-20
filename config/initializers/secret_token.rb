0f01e6764541242c7def3c2c388254a6ae9361aa
iorpath <iorpath@yahoo.com>
2012-09-20 06:44:01 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SistCoord::Application.config.secret_token = 'e229fc9e22c03a9f3a6ed263a95f1b1166e9e9f349a40f4eadef74e47258c01c321de6898cd7545e645e0a42d5003ad53ea2b9f60b7b98c1e14a227bbe43bbeb'
