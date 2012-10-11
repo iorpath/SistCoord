bfb0b1973ad1b654974730e7c2882a652ae13cd3
Carlos Diaz Maya <sunriken@gmail.com>
2012-10-10 19:07:49 -0500

    Ajustes hechos por felipe a la funcionalidad de materias vistas del estudiante

--- app/models/maestrium.rb
+++ app/models/maestrium.rb
@@ -2,11 +2,11 @@ class Maestrium < ActiveRecord::Base
   belongs_to :user
   has_many :pensums
   attr_accessible :codigo, :nombre, :user, :user_id
-  after_create :crear_pensum
+  #after_create :crear_pensum
   #Retorna el pensum vigente para esta maestria
   #Por ahora retorna el ultimo pensum inscrito
   def pensum_actual
-    return self.pensums.last
+    return Pensum.last
   end
 
   #Retorna las materias segun semestre sugerido
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
-puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
-puts 'New user created: ' << user2.name
-user.add_role :admin
+#sraiputs 'SETTING UP DEFAULT USER LOGIN'
+#user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+#puts 'New user created: ' << user.name
+#user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
+#puts 'New user created: ' << user2.name
+#user.add_role :admin
 
 if User.find_by_name("Felipe Test").nil?
   user = FactoryGirl.create :user, :name =>"Felipe Test", :email =>"gf.moyano131@uniandes.edu.co", :password => "geheim", :password_confirmation => 'geheim'
@@ -23,7 +23,7 @@ end
 
 maestria = FactoryGirl.create :maestrium, :nombre => "Maestria en Seguiridad Informatica", :codigo => "MESI"
 
-pensum = FactoryGirl.create :pensum
+pensum = FactoryGirl.create :pensum, :nombre => "2012-2"
 
 maestria.pensums << pensum
 maestria.save
@@ -33,7 +33,7 @@ estudiante.save
 
 8.times do |n|
   5.times do |m|
-    m = FactoryGirl.create :materium, :nombre => "materia #{m}"
+    m = FactoryGirl.create :materium, :nombre => "materia #{m}", :codigo => "MISIS10#{m}#{n}"
     pensum.materia_pensums.create :materium => m, :semestre_sugerido => n 
   end
 end
